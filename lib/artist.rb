176301745e65d1167bbfff458fb43197594457a2
Araba Taylor <araba.taylor20@gmail.com>
2017-11-08 02:51:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,11 @@ attr_accessor :name, :songs
     @songs = []
   end
 
-
   def add_song(song)
     @songs << song
     song.artist = self
   end
 
-  
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
