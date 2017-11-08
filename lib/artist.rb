a0f60b89ea42ce94f0543216ae1c687317e586f2
Araba Taylor <araba.taylor20@gmail.com>
2017-11-08 02:09:45 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+
+require 'pry'
+
+class Artist
+
+attr_accessor :name
+
+  def initialize(name)
+    @name=name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  binding.pry
+  def add_song_by_name(song)
+    song = Song.new(song)
+    @songs << song
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+
+attr_accessor :name, :artist
+
+  def initalize(name)
+    @name=name
+    @artist=Artist
+  end
+
+  def artist
+    self.artist
+  end
+
+end
